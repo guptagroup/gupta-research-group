9646d65dc499c536801f9db97db21c3c41f252d2
Sunny Gupta <sunnyguptatechiisc@gmail.com>
2026-08-19 14:50:54 -0400

    Configure sguptagroup.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://guptagroup.github.io',
-	base: '/gupta-research-group',
+	site: 'https://sguptagroup.com',
 });
