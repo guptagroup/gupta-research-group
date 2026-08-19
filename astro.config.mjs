e04a9502a1a96273e907727ae11b2d90bbb34b4d
Sunny Gupta <sunnyguptatechiisc@gmail.com>
2026-08-19 14:31:58 -0400

    Update GitHub username

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sunnygupta1993.github.io',
+	site: 'https://guptagroup.github.io',
 	base: '/gupta-research-group',
 });
